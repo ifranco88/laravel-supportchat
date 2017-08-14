04520aeb96e829202e40c8835f4816622387a79c
Ismael Franco <ifranco@devel8.com>
2017-08-14 19:02:45 +0200

    Change resource and config structure

--- src/SupportChatServiceProvider.php
+++ src/SupportChatServiceProvider.php
@@ -23,13 +23,23 @@ class SupportChatServiceProvider extends ServiceProvider
 
         //Publish Config
         $this->publishes([
-            __DIR__ . '/config' => config_path('laravel-supportchat'),
-        ]);
+            __DIR__ . '/../config/laravel-supportchat.php' => config_path('laravel-supportchat.php'),
+        ], 'config');
 
         //Publish Views
         $this->publishes([
-            __DIR__ . '/views' => base_path('resources/views/vendor/laravel-supportchat'),
-        ]);
+            __DIR__ . '/../resources/views' => base_path('resources/views/vendor/laravel-supportchat'),
+        ], 'views');
+
+        //Stylesheet
+        $this->publishes([
+            __DIR__ . '/../resources/assets/sass' => base_path('resources/assets/sass/vendor/laravel-supportchat'),
+        ], 'stylesheet');
+
+        //Javascript
+        $this->publishes([
+            __DIR__ . '/../resources/assets/js' => base_path('resources/assets/js/vendor/laravel-supportchat'),
+        ], 'javascript');
     }
 
     /**
@@ -46,7 +56,7 @@ class SupportChatServiceProvider extends ServiceProvider
 
         //Set config file and its alias
         $this->mergeConfigFrom(
-            __DIR__ . '/config/laravel-supportchat.php', 'laravel-supportchat'
+            __DIR__ . '/../config/laravel-supportchat.php', 'laravel-supportchat'
         );
     }
 }
